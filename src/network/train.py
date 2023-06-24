115983c0df7551209fe167d5e56968c352329631
adhi0331 <adhi03.ananthan@gmail.com>
2023-06-24 14:32:08 -0700

    updated model

--- src/network/train.py
+++ src/network/train.py
@@ -8,19 +8,17 @@ a model
 class ModelTrainer:
 
     # Method to train the model
-    def trainModel(self, epochs, model, optimizer, criterion, hrir_train, pos_train, anthro_train):
+    def trainModel(self, epochs, model, optimizer, criterion, hrir_train, anthro_train, pos_train):
         
-        x_train, d_train, theta_train = self.deconstructAnthro(anthro_train)
         losses = []
         for i in range(epochs):
             # propgate forward
-            x_pred, d_pred, theta_pred = model.forward(hrir_train, pos_train)
+            anthro_pred, pos_pred = model.forward(hrir_train)
 
             #calculate loss
-            lossX = criterion(x_pred, x_train) 
-            lossD = criterion(d_pred, d_train) 
-            lossTheta = criterion(theta_pred, theta_train)
-            totalLoss = lossX + lossD + lossTheta
+            lossAnthro = criterion(anthro_pred, anthro_train) 
+            lossPos = criterion(pos_pred, pos_train) 
+            totalLoss = lossAnthro + lossPos
 
             #Keep track of losses
             losses.append(totalLoss.detach().numpy())
@@ -33,11 +31,5 @@ class ModelTrainer:
             optimizer.zero_grad()
             totalLoss.backward()
             optimizer.step()
-    
-    #method to deconstruct anthro vector
-    def deconstructAnthro(anthroVector):
-        X = anthroVector[8:25]
-        D = anthroVector[0:8]
-        Theta = anthroVector[25:27]
-        return X, D, Theta
+
     
