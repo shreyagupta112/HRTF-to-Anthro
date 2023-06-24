03b0dcbacd09d2195270d691f994b7d7c7f27a64
adhi0331 <adhi03.ananthan@gmail.com>
2023-06-24 15:25:11 -0700

    added missing files

---
+++ src/model.py
@@ -0,0 +1,46 @@
+import torch 
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+'''
+This class represents the model architecture used for 
+this simple model
+
+Two input vectors:
+    HRIR of left ear: 1 X 200
+
+Output Vectors:
+    Anthro Measurements: 27 x 1
+    Position vector: 1 X 3
+
+'''
+
+class Model(nn.Module):
+
+    def __init__(self, 
+                 hrir=64, 
+                 h1=10, h2=15, h3=10, 
+                 anthro=27, pos=3):
+        super().__init__() # instantiate our nn.Module
+        #Connect model
+        self.fc1 = nn.Linear(hrir, h1)
+        self.fc2 = nn.Linear(h1, h2)
+        self.fc3 = nn.Linear(h2, h3)
+        self.fc_output1 = nn.Linear(h3, anthro)
+        self.fc_output2 = nn.Linear(h3, pos)
+    
+    # Function to propagate forward
+    def forward(self, hrir_l):
+
+        layer1 = F.relu(self.fc1(hrir_l))
+        layer2 = F.relu(self.fc2(layer1))
+        layer3 = F.relu(self.fc3(layer2))
+
+        Anthro = self.fc_output1(layer3)
+        Pos = self.fc_output2(layer3)
+
+        return Anthro, Pos
+
+
+
