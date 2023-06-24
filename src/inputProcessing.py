f18e6a7a4cc80a0afa7ee7852e7dc952a0589aaf
Shreya Gupta <sfgupta@ucsd.edu>
2023-06-24 12:47:21 -0700

    added func to extract anthro data in  inputProcessing.py

--- src/inputProcessing.py
+++ src/inputProcessing.py
@@ -23,10 +23,24 @@ class InputProcessing:
         
         return row
     
+    def extractAnthro(subject_num):
+        subject = 'subject_' + str(subject_num).zfill(3)
+        file_path =  os.path.join('..','data','cipic.hdf5')
+
+        with h5py.File(file_path, "r") as f:
+            dset = f[subject]
+            d = np.array(dset.attrs['D'])
+            x = np.array(dset.attrs['X'])
+            theta = np.array(dset.attrs['theta'])
+            row = np.hstack((d, x, theta))
+            
+        
+        return row
+    
     subject_number = 3
     row_index = 0
     hrir = extractRowFromHRIR(row_index, subject_number)
     position = extractPos(row_index, subject_number)
-    print(hrir)
-    print(position)
+    anthro = extractAnthro(subject_number)
+    print(anthro)
         
