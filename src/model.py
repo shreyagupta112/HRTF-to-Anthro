5b8470a09ea97b5ebfb720f6e121de3b3cea056a
adhi0331 <adhi03.ananthan@gmail.com>
2023-07-17 22:15:11 +0530

    converted to trunc64

--- src/model.py
+++ src/model.py
@@ -18,7 +18,7 @@ Output Vectors:
 class Model(nn.Module):
 
     def __init__(self, 
-                 hrir_pos=203, 
+                 hrir_pos=67, 
                  h1=10, h2=15, h3=10, 
                  ear_anthro=10):
         super().__init__() # instantiate our nn.Module
