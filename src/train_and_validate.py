1f55b38ce81241ad73898084a1bce2ed2ba28cb3
Shreya Gupta <sfgupta@ucsd.edu>
2023-07-21 10:30:17 -0700

    break into 3 data sets instead of 2

--- src/train_and_validate.py
+++ src/train_and_validate.py
@@ -15,7 +15,13 @@ class ModelTrainer:
         hrir_pos = np.hstack((hrir, pos))
         anthro = InputProcessing.extractAnthro(3, True)
         anthro = np.tile(anthro, (1250,1))
-        hrir_pos_train,  hrir_pos_test, anthro_train, anthro_test = train_test_split(hrir_pos, anthro, test_size=0.2, random_state=41)
+        # Split the data into training and test sets (70% training, 30% test)
+        hrir_pos_train, hrir_pos_test, anthro_train, anthro_test = train_test_split(
+            hrir_pos, anthro, test_size=0.3, random_state=41)
+
+        # Split the test set again to get validation set (20% validation, 10% test)
+        hrir_pos_valid, hrir_pos_test, anthro_valid, anthro_test = train_test_split(
+            hrir_pos_test, anthro_test, test_size=0.33, random_state=41)
 
         # get mean and standard deviation
         self.anthro_mean = np.mean(anthro)
@@ -28,16 +34,22 @@ class ModelTrainer:
         self.X_train = (hrir_pos_train - self.hrirPos_mean) / (self.hrirPos_std)
         hrir_pos_test = torch.FloatTensor(hrir_pos_test)
         self.X_test  = (hrir_pos_test - self.hrirPos_mean) / (self.hrirPos_std)
+        hrir_pos_valid = torch.FloatTensor(hrir_pos_valid)
+        self.X_valid  = (hrir_pos_valid - self.hrirPos_mean) / (self.hrirPos_std)
         anthro_train = torch.FloatTensor(anthro_train)
         self.anthro_train = (anthro_train - self.anthro_mean) / (self.anthro_std)
         anthro_test = torch.FloatTensor(anthro_test)
         self.anthro_test = (anthro_test - self.anthro_mean) / (self.anthro_std)
+        anthro_valid = torch.FloatTensor(anthro_valid)
+        self.anthro_valid = (anthro_valid - self.anthro_mean) / (self.anthro_std)
 
     # Method to train the model
     def trainModel(self, model):
         # Get training data
         X_train = self.X_train
         anthro_train = self.anthro_train
+        X_test = self.X_test
+        anthro_test = self.anthro_test
         # Set loss function
         criterion = nn.MSELoss()
         #Choose Adam Optimizer, learning rate
@@ -60,18 +72,21 @@ class ModelTrainer:
             indivLosses.append(mse.detach().numpy())
             #Keep track of losses
             losses.append(lossAnthro.detach().numpy())
-
             train_output = torch.mean(mse, dim=0)
             mse_train_data.append(np.array(train_output.detach().numpy()))
 
             if i % 10 == 0:
                 print(f'Epoch: {i} and loss: {lossAnthro}')
             
+            #Do some backward propagation
+            model.train()
+            optimizer.zero_grad()
+            lossAnthro.backward()
+            optimizer.step()
+
             # Get the MSE of the validation data without chaning the wieghts
             model.eval()   
             with torch.no_grad():
-                X_test = self.X_test
-                anthro_test = self.anthro_test
                 anthro_val_pred = model.forward(X_test)
 
                 loss_fn = nn.MSELoss()
@@ -80,15 +95,6 @@ class ModelTrainer:
                     val_output[column_index] = loss_fn(anthro_val_pred[:, column_index], anthro_test[:, column_index])
             mse_validation_data.append(val_output)
     
-
-
-            #Do some backward propagation
-            model.train()
-            optimizer.zero_grad()
-            lossAnthro.backward()
-            optimizer.step()
-
-            # Calculate mean squared error
         '''
 
         # Plot losses
