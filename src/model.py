381f3ccfbfe4de04cd3504c45ad77905e0f906d2
adhi0331 <adhi03.ananthan@gmail.com>
2023-07-28 13:17:51 +0530

    increased model params

--- src/model.py
+++ src/model.py
@@ -19,14 +19,20 @@ class Model(nn.Module):
 
     def __init__(self, 
                  hrir_pos=36, 
-                 h1=30, h2=45, h3=30, 
+                 h1=40, h2=50, h3=60, h4=70, h5=80, h6=65, h7=45, h8=25, h9=15,
                  ear_anthro=10):
         super().__init__() # instantiate our nn.Module
         #Connect model
         self.fc1 = nn.Linear(hrir_pos, h1)
         self.fc2 = nn.Linear(h1, h2)
         self.fc3 = nn.Linear(h2, h3)
-        self.fc_output = nn.Linear(h3, ear_anthro)
+        self.fc4 = nn.Linear(h3, h4) 
+        self.fc5 = nn.Linear(h4, h5)
+        self.fc6 = nn.Linear(h5, h6)
+        self.fc7 = nn.Linear(h6, h7)
+        self.fc8 = nn.Linear(h7, h8)
+        self.fc9 = nn.Linear(h8, h9)
+        self.fc_output = nn.Linear(h9, ear_anthro)
     
     # Function to propagate forward
     def forward(self, hrir_l):
@@ -34,8 +40,14 @@ class Model(nn.Module):
         layer1 = F.relu(self.fc1(hrir_l))
         layer2 = F.relu(self.fc2(layer1))
         layer3 = F.relu(self.fc3(layer2))
+        layer4 = F.relu(self.fc4(layer3))
+        layer5 = F.relu(self.fc5(layer4))
+        layer6 = F.relu(self.fc6(layer5))
+        layer7 = F.relu(self.fc7(layer6))
+        layer8 = F.relu(self.fc8(layer7))
+        layer9 = F.relu(self.fc9(layer8))
 
-        Ear_Anthro = self.fc_output(layer3)
+        Ear_Anthro = self.fc_output(layer9)
 
         return Ear_Anthro
 
